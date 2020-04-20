34f6043354addc668f3ad304eefbc32584f0274c
sarah wang <sarahwang47@gmail.com>
2020-04-20 14:04:56 +0800

    the new article form is working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_082448) do
+ActiveRecord::Schema.define(version: 2020_04_20_053805) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
